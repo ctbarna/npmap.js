8d9fe99bff4bcf8c0c9089a8eae5081831e0b689
Jim McAndrew <jim@loc8.us>
2013-10-15 00:43:49 -0600

    Create seperate UTFgrid handling file

---
+++ src/util/utfgrid.js
@@ -0,0 +1,59 @@
+var reqwest = require('../util/cachedreqwest')();
+
+module.exports = {
+  getTileGridPoint: function _getTileGridPoint(latLng, result, layer) {
+
+    // Forked from danzel/Leaflet.utfgrid
+    // https://github.com/danzel/Leaflet.utfgrid/blob/master/src/leaflet.utfgrid.js
+    var me = this,
+    point = layer._map.project(latLng),
+    tileSize = layer.options.tileSize || 256,
+    resolution = layer.options.resolution || 4,
+    x = Math.floor(point.x / tileSize),
+    y = Math.floor(point.y / tileSize),
+    max = layer._map.options.crs.scale(layer._map.getZoom()) / tileSize,
+    returnValue;
+
+    x = (x + max) % max;
+    y = (y + max) % max;
+
+    var gridX = Math.floor((point.x - (x * tileSize))/ resolution),
+    gridY = Math.floor((point.y - (y * tileSize)) / resolution),
+    key = result.grid[gridY].charCodeAt(gridX);
+
+    // Return the data from the key
+    returnValue = (result.data[result.keys[me.utfDecode(key)]]);
+    returnValue = returnValue ? returnValue : {'Error': 'No Data Found'};
+    return returnValue;
+  },
+  getTileGrid: function (tileUrl, latLng, layer, callback) {
+    var me = this;
+    reqwest.cachedReqwest({
+      url: tileUrl,
+      type: 'jsonp',
+      success: function (res) {
+        callback(me.getTileGridPoint(latLng, res, layer));
+      },
+      error: function (err) {
+        callback({'Error': 'No Data Found'});
+      }
+    });
+  },
+  utfDecode: function _utfDecode(key) {
+    // https://github.com/danzel/Leaflet.utfgrid/blob/master/src/leaflet.utfgrid.js
+    if (key >= 93) key--;
+    if (key >= 35) key--;
+    return key - 32;
+  },
+  hasUtfData: function(url, latLng, layer) {
+    var returnValue = {'cursor': 'default'};
+    if (reqwest.getCache(url)) {
+      if (reqwest.getCache(url).status === 'success') {
+        returnValue = this.getTileGridPoint(latLng, reqwest.getCache(url).resp, layer).Error ? false : {'cursor': 'pointer'};
+      } else if (reqwest.getCache(url).status === 'error') {
+        returnValue = false;
+      }
+    }
+    return returnValue;
+  }
+};
