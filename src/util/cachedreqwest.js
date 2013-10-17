abdd2aad19e1042241469eb30e0ad2941aa670f0
Jim McAndrew <jim@loc8.us>
2013-10-16 21:31:52 -0600

    Fix changes to allow for json

--- src/util/cachedreqwest.js
+++ src/util/cachedreqwest.js
@@ -3,7 +3,7 @@
 
 'use strict';
 var reqwest = require('reqwest'),
-  defaultTimeout = 3000;
+defaultTimeout = 3000;
 
 module.exports = function (options) {
   var cache = {},
@@ -13,16 +13,21 @@ module.exports = function (options) {
     // Loops through all the promises and fulfills them
     var response = cache[url];
     for (var promiseIndex in promises[url]) {
-      if (promises[url][promiseIndex][response.status]) {
-        promises[url][promiseIndex][response.status](response.resp);
+      if (promises[url][promiseIndex][response.cacheStatus]) {
+        promises[url][promiseIndex][response.cacheStatus](response);
+        if (promises[url][promiseIndex].complete) {
+          promises[url][promiseIndex].complete(response);
+        }
       }
     }
     delete promises[url];
   },
   checkTimeout = function (url) {
     // Checks how long we're been waiting for a response, and if it's too long, we give up
-    if ((new Date() - cache[url].startTime) > timeout) {
-      cache[url] = {'status': 'error', 'resp': {'error': 'timeout'}};
+    if (cache[url].cacheStatus === 'waiting' && (new Date() - cache[url].startTime) > timeout) {
+      cache[url].cacheStatus = 'error';
+      cache[url].statusText = 'Request Timeout';
+      cache[url].status = -408;
       fulfillPromises(url);
     }
   },
@@ -32,7 +37,7 @@ module.exports = function (options) {
 
     // Store the callbacks as promises
     promises[options.url] = promises[options.url] ? promises[options.url] : [];
-    promises[options.url].push({'success': options.success, 'error': options.error});
+    promises[options.url].push({'success': options.success, 'error': options.error, 'complete': options.complete});
 
     if (cache[options.url]) {
       // We already called it once
@@ -43,15 +48,26 @@ module.exports = function (options) {
         fulfillPromises(options.url);
       }
     } else {
-      cache[options.url] = {'status': 'waiting', 'resp': null, 'startTime': new Date()};
+      cache[options.url] = {'cacheStatus': 'waiting', 'response': null, 'startTime': new Date()};
 
-      // Change the response functions
-      newOptions.success = function(resp) {
-        cache[options.url] = {'status': 'success', 'resp': resp};
-        fulfillPromises(options.url);
-      };
-      newOptions.error = function(err) {
-        cache[options.url] = {'status': 'error', 'resp': {'error': err}};
+      // remove the old callbacks
+      delete newOptions.success;
+      delete newOptions.error;
+      delete newOptions.complete;
+
+      // Create a new one to encapsulate them all!
+      newOptions.complete = function(response) {
+        if (options.type === 'jsonp') {
+          cache[options.url].cacheStatus = response ? 'success' : 'error';
+          cache[options.url].response = response;
+          cache[options.url].statusText = "OK";
+          cache[options.url].status = 200;
+        } else if (options.type === 'json'){
+          cache[options.url].cacheStatus = response.status === 200 ? 'success' : 'error';
+          cache[options.url].response = response.response;
+          cache[options.url].statusText = response.statusText;
+          cache[options.url].status = response.status;
+        }
         fulfillPromises(options.url);
       };
 
