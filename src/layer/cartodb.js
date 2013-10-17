059c45c9d6d34fdf342f841cf5a3f24de3c0529c
Jim McAndrew <jim@loc8.us>
2013-10-17 13:44:16 -0600

    Get cartodb SQL and INTERACTIVITY parameters to work (needs cleaning up)

--- src/layer/cartodb.js
+++ src/layer/cartodb.js
@@ -1,10 +1,12 @@
 /* global L */
 /* global document */
 /* jslint node: true */
+/* jshint camelcase: false */
 
 'use strict';
 
 var util = require('../util/util'),
+reqwest = require('../util/cachedreqwest')().cachedReqwest,
 utfGrid, UtfGrid = require('../util/utfgrid');
 
 
@@ -50,29 +52,46 @@ var CartoDbLayer = L.TileLayer.extend({
     util.strict(this.options.table, 'string');
     util.strict(this.options.user, 'string');
 
-    this.options.rootUrl = [
-      this.options.protocal,
-      '//',
-      this.options.user,
-      '.',
-      this.options.domain,
-      '/',
-      this.options.tileDirectory,
-      '/',
-      this.options.table,
-      '/',
-      this.options.tileXYZ
-    ].join('');
-    this.options.url = [
-      this.options.rootUrl,
-      '.',
-      this.options.format
-    ].join('');
-    this.options.grids = [
-      this.options.rootUrl,
-      '.',
-      this.options.gridFormat
-    ].join('');
+    this.options.urls = {};
+    this.options.urls.root = [this.options.protocal, '//', this.options.user, '.', this.options.domain].join('');
+    this.options.urls.rootTile = [this.options.urls.root, '/', this.options.tileDirectory, '/', this.options.table, '/', this.options.tileXYZ].join('');
+    this.options.url = [this.options.urls.rootTile, '.', this.options.format].join('');
+    this.options.grids = [this.options.urls.rootTile, '.', this.options.gridFormat].join('');
+
+
+    // Probably break these into their own functions
+    // Get the sql statement
+    //Sample: SELECT * FROM scpn_weather_stations LIMIT 1
+    //https://nps-scpn.cartodb.com/api/v2/sql?q=SELECT%20*%20FROM%20scpn_weather_stations%20LIMIT%201
+    var me = this;
+    this.options.urls.api = [this.options.urls.root.replace('http://', 'https://'), '/', 'api', '/', 'v2', '/', 'sql'].join('');
+    var sqlQuery = ['SELECT * FROM ', this.options.table, ' LIMIT 1;'].join(''),
+    apiUrl = this.buildUrl(this.options.urls.api, {'q': sqlQuery});
+    console.log(apiUrl);
+    reqwest({
+      url: apiUrl,
+      type: 'jsonp',
+      success: function(response) {
+        console.log('success', response);
+        // Parse out that SQL;
+        console.log(response.response);
+        var interactivity = [], sql=[];
+        for (var field in response.response.fields) {
+          if (response.response.fields[field].type === 'string' || response.response.fields[field].type === 'number') {
+            interactivity.push(field);
+          }
+        }
+        me.options.interactivity = me.options.interactivity ?  me.options.interactivity : interactivity.join(',');
+        var theGeom = response.response.fields.the_geom_webmercator ? 'the_geom_webmercator' : 'the_geom as the_geom_webmercator';
+        sql = ['SELECT ', me.options.interactivity, ',', theGeom, ' FROM ', me.options.table, ';'];
+        me.options.sql = me.options.sql ? me.options.sql : sql.join('');
+      },
+      error: function(response) {
+        console.log('error', response);
+      }
+    });
+
+
 
     L.TileLayer.prototype.initialize.call(this, this.options.url, this.options);
     utfGrid = new UtfGrid(this, {'crossOrigin': true, 'type': 'jsonp'});
@@ -81,8 +100,10 @@ var CartoDbLayer = L.TileLayer.extend({
   _getTileGridUrl: function (latLng) {
     var me = this,
     gridTileCoords = utfGrid.getTileCoords(latLng),
-    grids = me.options.grids;
-    return L.Util.template(grids, gridTileCoords) + '?sql=' + 'SELECT online,park,station_na,data_start,data_end,datewprese,station_id,network,lat,long,elevm,in_park,state,county,hcnm_num,gov_admin,note,for_data,icon,url,_order,field_22,the_geom,cartodb_id,created_at,updated_at,the_geom_webmercator,ST_ASGEOJSON(the_geom) as geometry FROM scpn_weather_stations&interactivity=online,park,station_na,data_start,data_end,datewprese,station_id,network,lat,long,elevm,in_park,state,county,hcnm_num,gov_admin,note,for_data,icon,url,_order,field_22,the_geom,cartodb_id,created_at,updated_at,the_geom_webmercator,geometry';
+    grids = me.options.grids,
+    baseUrl = L.Util.template(grids, gridTileCoords),
+    params = {'sql': me.options.sql, 'interactivity': me.options.interactivity};
+    return me.buildUrl (baseUrl, params);
   },
   _isQueryable: function(latLng) {
     var returnValue = false,
@@ -112,6 +133,26 @@ var CartoDbLayer = L.TileLayer.extend({
   },
   onRemove: function onRemove() {
     L.TileLayer.prototype.onRemove.call(this, this._map);
+  },
+  buildUrl: function (base, params) {
+    // This should probably be moved elsewhere
+    var returnArray = [];
+
+    if(params) {
+      returnArray.push(base + '?');
+    } else {
+      return base;
+    }
+    for (var param in params) {
+      returnArray.push(encodeURIComponent(param));
+      returnArray.push('=');
+      returnArray.push(encodeURIComponent(params[param]));
+      returnArray.push('&');
+    }
+    // Remove trailing '&'
+    returnArray.pop();
+
+    return returnArray.join('');
   }
 });
 
