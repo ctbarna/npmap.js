3ce7d4b86b8ca08fb2a55c50755c30e4a14b2ca9
Nate Irwin <nate@nateirwin.net>
2013-11-03 11:19:25 -0700

    Add support for GeoJSON layers

---
+++ src/layer/geojson.js
@@ -0,0 +1,72 @@
+/* global L */
+
+'use strict';
+
+var reqwest = require('reqwest'),
+    util = require('../util/util');
+
+var GeoJsonLayer = L.GeoJSON.extend({
+  includes: [
+    require('../util/geojson')
+  ],
+  initialize: function(config) {
+    this._config = this._toLeaflet(config);
+
+    if (typeof config.data === 'object') {
+      L.GeoJSON.prototype.initialize.call(this, config.data, config);
+      this._addAttribution();
+      return this;
+    } else {
+      var me = this;
+
+      util.strict(config.url, 'string');
+
+      if (config.url.indexOf('http://') === -1 && config.url.indexOf('https://') === -1) {
+        reqwest({
+          error: function(error) {
+            console.log('The GeoJSON layer cannot be loaded. Error: ' + error + '.');
+          },
+          success: function(response) {
+            L.GeoJSON.prototype.initialize.call(me, response, me._config);
+            me._addAttribution();
+            return me;
+          },
+          type: 'json',
+          url: config.url
+        });
+      } else {
+        reqwest({
+          crossOrigin: true,
+          error: function() {
+            console.log('The GeoJSON layer cannot be loaded via CORS. Will now try JSONP.');
+
+            try {
+              reqwest({
+                success: function(response) {
+                  L.GeoJSON.prototype.initialize.call(me, response, me._config);
+                  me._addAttribution();
+                  return me;
+                },
+                type: 'jsonp',
+                url: config.url
+              });
+            } catch (exception) {
+              console.log('The GeoJSON layer cannot be loaded via JSONP. Perhaps you need to add a callback parameter to the URL config property?');
+            }
+          },
+          success: function(response) {
+            L.GeoJSON.prototype.initialize.call(me, response, me._config);
+            me._addAttribution();
+            return me;
+          },
+          type: 'json',
+          url: config.url
+        });
+      }
+    }
+  }
+});
+
+module.exports = function(config) {
+  return new GeoJsonLayer(config);
+};
