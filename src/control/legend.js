0d8f1b7cb3193d65603cf0d825d08f6ab0f086a6
Nate Irwin <nate@nateirwin.net>
2014-01-07 12:04:08 -0700

    A few tweaks to how control is configured

--- src/control/legend.js
+++ src/control/legend.js
@@ -1,17 +1,23 @@
 /* globals L */
 
 var LegendControl = L.Control.extend({
-  options: {},
+  options: {
+    position: 'topright'
+  },
   _html: null,
   initialize: function(options) {
     L.Util.setOptions(this, options);
     this._container = L.DomUtil.create('div', 'leaflet-control-legend');
     L.DomEvent.disableClickPropagation(this._container);
-    
-    if (typeof options.html === 'string') {
-      this._html = options.html;
-    } else if (typeof options.html === 'function') {
-      this._html = options.html();
+
+    if (options.html) {
+      if (typeof options.html === 'string') {
+        this._html = options.html;
+      } else if (typeof options.html === 'function') {
+        this._html = options.html();
+      }
+    } else if (options.overlays) {
+      this._html = this._createLegend(options.overlays);
     }
 
     if (this._html) {
@@ -19,14 +25,71 @@ var LegendControl = L.Control.extend({
     }
   },
   onAdd: function(map) {
+    this._map = map;
+
     if (!this._html) {
-      if (map.options.overlays.length) {
-        // TODO: Iterate through overlays and create legend.
-      }
+      // TODO: Add 'ready' event to map, then iterate through all baselayers and shapes, per individual overlay, on the map, dynamically building a legend.
     }
 
     return this._container;
   },
+  _createLegend: function(overlays) {
+    var html = '',
+      options = this.options;
+
+    if (options.title) {
+      html += '<h4>' + options.title  + '</h4>';
+    }
+
+    for (var i = 0; i < overlays.length; i++) {
+      var overlay = overlays[i];
+
+      if (overlay.name) {
+        html += '<h5>' + overlay.name + '</h5>';
+      }
+
+      if (overlay.icons) {
+        html += '<ul>';
+
+        for (var icon in overlay.icons) {
+          html += '<li><span style="background-color:' + overlay.icons[icon]  + ';">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span> ' + icon + '</li>';
+        }
+      }
+
+      /*
+      if (overlay.clustered) {
+        var bottomValue = 0,
+          clusterHtml = '<h6>Groups</h6>',
+          lastColor = '',
+          upperValue = 0;
+
+        for (var group = 0; group < options.layers[layer].clustered.length; group++) {
+          if (lastColor && options.layers[layer].clustered[group].color !== lastColor) {
+            if (!lastColor.match(/^#/g)) {lastColor = '#' + lastColor;}
+            clusterHtml += '<span style="background-color: ' + lastColor  + '; border-radius: 8px;">&nbsp;&nbsp;&nbsp;&nbsp;</span> ' + bottomValue + ' - ' + upperValue + ' points</br>';
+            bottomValue = upperValue + 1;
+          }
+          upperValue = options.layers[layer].clustered[group].maxNodes;
+          lastColor = options.layers[layer].clustered[group].color;
+        }
+
+        if (!lastColor.match(/^#/g)) {
+          lastColor = '#' + lastColor;
+        }
+
+        if (bottomValue === 0) {
+          clusterHtml = '<span style="background-color: ' + lastColor  + '; border-radius: 8px;">&nbsp;&nbsp;&nbsp;&nbsp;</span> Grouped Points</br>';
+        } else {
+          clusterHtml += '<span style="background-color: ' + lastColor  + '; border-radius: 8px;">&nbsp;&nbsp;&nbsp;&nbsp;</span> &gt; ' + bottomValue + ' points</br>';
+        }
+
+        html += clusterHtml;
+      }
+      */
+    }
+
+    return html;
+  }
   /*
   _update: function() {
     function cssString(css) {
@@ -59,60 +122,6 @@ var LegendControl = L.Control.extend({
 
     return this._update();
   },
-  _createLegend: function(options) {
-    var html = '';
-
-    if (options.title) {
-      html += '<h4>' + options.title  + '</h4>';
-    }
-
-    if (options.layers) {
-      for (var layer in options.layers) {
-        if (options.layers[layer].name) {
-          html += '<h6>' + options.layers[layer].name + '</h6>';
-        }
-
-        if (options.layers[layer].makiIcons) {
-          for (var icon in options.layers[layer].makiIcons) {
-            html += '<span style="background-color: ' + options.layers[layer].makiIcons[icon]  + ';">&nbsp;&nbsp;&nbsp;&nbsp;</span> ' + icon + '</br>';
-          }
-        }
-
-        if (options.layers[layer].clustered) {
-          var bottomValue = 0,
-            clusterHtml = '',
-            lastColor = '',
-            upperValue = 0;
-
-          clusterHtml += '<h6>Groups</h6>';
-
-          for (var group = 0; group < options.layers[layer].clustered.length; group++) {
-            if (lastColor && options.layers[layer].clustered[group].color !== lastColor) {
-              if (!lastColor.match(/^#/g)) {lastColor = '#' + lastColor;}
-              clusterHtml += '<span style="background-color: ' + lastColor  + '; border-radius: 8px;">&nbsp;&nbsp;&nbsp;&nbsp;</span> ' + bottomValue + ' - ' + upperValue + ' points</br>';
-              bottomValue = upperValue + 1;
-            }
-            upperValue = options.layers[layer].clustered[group].maxNodes;
-            lastColor = options.layers[layer].clustered[group].color;
-          }
-
-          if (!lastColor.match(/^#/g)) {
-            lastColor = '#' + lastColor;
-          }
-
-          if (bottomValue === 0) {
-            clusterHtml = '<span style="background-color: ' + lastColor  + '; border-radius: 8px;">&nbsp;&nbsp;&nbsp;&nbsp;</span> Grouped Points</br>';
-          } else {
-            clusterHtml += '<span style="background-color: ' + lastColor  + '; border-radius: 8px;">&nbsp;&nbsp;&nbsp;&nbsp;</span> &gt; ' + bottomValue + ' points</br>';
-          }
-
-          html += clusterHtml;
-        }
-      }
-    }
-
-    return html;
-  }
   */
 });
 
